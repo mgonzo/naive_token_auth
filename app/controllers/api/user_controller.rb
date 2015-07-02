81d36cea48ada553985f02df382480a9677d1617
mgonzo <mgonzomatic@gmail.com>
2015-07-02 16:46:27 -0700

    No model AR::Concern, Just using model
    Just gonna put everything in the model until
    I need to do otherwise.  Seems like concern may
    not be the best way to go.

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -1,68 +1,43 @@
 class Api::UserController < Api::BaseController
   require 'uuid'
-  require 'openssl'
-  require 'base64'
 
   def index
     render json: { :msg => 'hello index' }
   end
 
   def create
-    name = params[:name]
-    email = params[:email]
-    password1 = params[:password1]
-    password2 = params[:password2]
+    # does user exist already?
+    # if yes, send user to sign in
+    # else create user
 
     # sanitize everything
     # is everything present?
     # is email good?
     # do passwords match?
+    name = params[:name]
+    email = params[:email]
+    password = params[:password1]
     
-    # does user exist already?
-    # if yes, just sign user in
-    # else create user
-
-    # USER
-    # create a user_id uuid
-    uuid = UUID.new 
-    user_id = uuid.generate :compact
-
-    # get hashed password
-    hash = OpenSSL::HMAC.digest('sha256', ENV['SIMPLE_TOKEN_AUTH_KEY_BASE'], params[:password1])
-    hashed_password = Base64.encode64(hash)
-
-    # DATE TIME
-    # get current date time
-    current_date = Date.current
-
     # save user
     safe_params = {
       :name => params[:name],
       :email => params[:email],
-      :user_id => user_id,
-      :password => hashed_password,
-      :current_sign_in_at => current_date,
-      :last_sign_in_at => current_date
+      :password => password,
     }
 
+    # create user
     @user = User.new(safe_params);
 
+    # create response
     if (@user.save)
-      # create response
       render json: { :token => @user.current_token }
     else
       render json: { :message => 'unable to save user'}
     end
 
-
-
     # if anything fails, send back crap
     # http code and whatever else
     # redirect to index or error page
   end
-
-  private 
-    def user_params
-    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,38 @@
 class User < ActiveRecord::Base
-  include Tokenizable
+  require 'openssl'
+  require 'base64'
   require 'jwt'
 
-  before_save :generate_token
+  before_create :generate_uuid
+  before_create :generate_password
 
+  before_save :generate_token
+  before_save :set_datetime
+
+  # SET NEW TOKEN
   def generate_token
     self.current_token = JWT.encode({ user_id: self.user_id }, ENV['SIMPLE_TOKEN_AUTH_KEY_BASE'])
   end
+
+  # SET DATE TIME
+  def set_datetime
+    # get current date time
+    current_date = Date.current
+    self.current_sign_in_at = current_date
+    self.last_sign_in_at = current_date
+  end
+
+  # CREATE PASSWORD
+  def generate_password
+    hash = OpenSSL::HMAC.digest('sha256', ENV['SIMPLE_TOKEN_AUTH_KEY_BASE'], self.password)
+    self.password = Base64.encode64(hash)
+  end
+
+  # CREATE UUID
+  def generate_uuid
+    # create a user_id uuid
+    uuid = UUID.new 
+    self.user_id = uuid.generate :compact
+  end
+
 end
