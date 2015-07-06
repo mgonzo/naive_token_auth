ee3f4ddcd4aff1024245462925095ce2681f306d
mgonzo <mgonzomatic@gmail.com>
2015-07-05 19:51:14 -0700

    Moved more work into the module
    I think it's pretty good now in terms of the module
    is doing the auth work and there's not much code for
    the app model and controller to handle.  Maybe could
    be minimized more still, but it's almost there.

--- app/models/concerns/tokenize.rb
+++ app/models/concerns/tokenize.rb
@@ -12,13 +12,13 @@ module Tokenize extend ActiveSupport::Concern
     end
 
     # Generate a hashed password from plain text
-    def password_generate (password)
-      Base64.encode64(OpenSSL::HMAC.digest('sha256', ENV['SIMPLE_TOKEN_AUTH_KEY_BASE'], password))
+    def password_generate (string_to_hash)
+      Base64.encode64(OpenSSL::HMAC.digest('sha256', ENV['SIMPLE_TOKEN_AUTH_KEY_BASE'], string_to_hash))
     end
 
     # Generate a new token
     def token_generate (id)
-      JWT.encode({ user_id: id }, ENV['TOKENIZE_KEY'])
+      JWT.encode({ user_id: id }, ENV['SIMPLE_TOKEN_AUTH_KEY_BASE'])
     end
 
     # Set current and last tokens
@@ -31,8 +31,38 @@ module Tokenize extend ActiveSupport::Concern
     # user id, hashed password, current token
     def create_user (model)
       model.user_id = self.uuid_generate
-      model.password = self.password_generate(model.password)
+      model.password = self.password_generate(model.password + model.user_id)
       model.current_token = self.token_generate(model.user_id)
+    end
+
+    # SET DATE TIME
+    def set_datetime (model)
+      model.last_sign_in_at = model.current_sign_in_at
+      model.current_sign_in_at = Date.current
+    end
+
+    # handle password verification
+    # authentication, 
+    # and token set
+    def auth_signin (model, password)
+      hashed_password = self.password_generate(password + model.user_id)
+      @user_by_password = model.class.find_by password: hashed_password
+
+      if (!@user_by_password)
+        return nil
+      end
+
+      # match internal id's to be sure
+      # that this is the same user record
+      if (@user_by_password.id === model.id)
+        self.token_swap(model)
+        self.set_datetime(model)
+        model.save
+        return model
+      else
+        return nil
+      end
+
     end
 
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,60 +1,37 @@
 class User < ActiveRecord::Base
   include Tokenize
-
   validates_uniqueness_of :name
-
   before_create :setup_auth
 
+  #
+  # setup_auth
+  #
+  # call module create_user
+  # to setup password, user_id, tokens
   def setup_auth
     self.class.create_user(self)
   end
 
-  # SET DATE TIME
-  def set_datetime
-    self.last_sign_in_at = self.current_sign_in_at
-    self.current_sign_in_at = Date.current
-  end
-
-  # instance signin
-  def signin
-    # just passing the user instance right in
-    self.class.token_swap(self)
-    self.set_datetime
-    self.save
-    return self
-  end
-
-  # is a class function
-  # probably belongs in the mixin
+  #
+  # User.signin
+  #
+  # find a user and
+  # try to signin the user
+  # with the auth module
   def self.signin (name_or_email, password)
-    # if password is nil give up
-    # passwd hash
-    hashed_password = self.password_generate(password)
+    # find a user by name or by email
+    # to match up with the password
+    @user = self.find_by name: name_or_email
 
-    # find a user by name, email and by password
-    @user_by_password = self.find_by password: hashed_password
-    if (!@user_by_password)
+    if (!@user) 
+      @user = self.find_by email: name_or_email
+    end
+
+    if (!@user) 
       return nil
     end
 
-    @user_by_name = self.find_by name: name_or_email
-    @user_by_email = self.find_by name: name_or_email
-    if (!@user_by_name && !@user_by_email)
-      return nil
-    end
-
-    # did all of these return a User?
-    # match internal id on either name/password or email/password
-    case @user_by_password.id
-    when @user_by_name.id
-      return @user_by_name.signin
-
-    when @user_by_email.id
-      return @user_by_email.signin
-
-    else
-      return nil
-    end
+    return self.auth_signin(@user, password)
   end
 
 end
