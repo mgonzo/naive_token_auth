e80fcaf95f808a88a55497571d3d270bf2dabcf1
mgonzo <marcos.gonzales@sheknows.com>
2015-07-15 17:48:48 -0700

    just committing some comments and stuff
    still working it out as far as the base
    controller auth goes.

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -14,4 +14,37 @@ class Api::BaseController < ApplicationController
   #
   # put it here first,
   # then move it to a concern
+  before_filter :parse_request, :auth_from_token!
+
+  def parse_request
+    # is every request supposed to be a post?
+    # what about get requests?
+    # can use get params like Ooyala does
+    #
+    # if this is a post, put or patch api_token should be in body
+    # if this is a get api_token should be a query param
+    #
+    # what if we use an http header for the api_token?
+    # anything wrong with that?
+    # then it will always be the same ...
+    #
+    # request.headers["Authorization"]
+    # http://stackoverflow.com/questions/8463809/customize-the-authorization-http-header
+
+    if (request.request_method == 'GET')
+      # get the param
+      @token = request.query_parameters['api_token']
+    else
+      @json = JSON.parse(request.body.read)
+      @token = @json['api_token']
+    end
+  end
+
+  def auth_from_token!
+    @user = nil
+
+    User.find_each do |u|
+    end
+  end
+
 end
