86ea85a703258ebd66915d3ec8c3e335fc3344b7
mgonzo <mgonzomatic@gmail.com>
2016-01-21 23:05:45 -0700

    Added some unit tests
    Added unit tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # User name should be unique
+  test "should have unique name" do
+    @params = {
+      :name => 'mgonzo4',
+      :email => 'mgonzo@mail.com',
+      :password => 'pass1'
+    }
+    user = User.create(@params)
+    assert user.valid?, "user name is not unique"
+  end
+
+  # test user signin
+  # should return nil if wrong password
+  test "signin should return nil wrong password" do
+    @name = 'mgonzo1',
+    @password = 'wrongpassword'
+    assert_nil User.signin(@name, @password)
+  end
+
+  # test user signin
+  # should return nil if no user found
+  test "signin should return nil no user" do
+    @name = 'mgonzo_not_created',
+    @password = 'password'
+    assert_nil User.signin(@name, @password)
+  end
+
+  # test user signout
+  test "signout" do
+    assert false
+  end
 end
